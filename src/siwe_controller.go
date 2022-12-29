6f68883725ad5159b04940b2615952d0a83cf9c4
Vadim Filimonov <fffilimonov@yandex.ru>
2022-12-29 14:55:49 +0200

    verify nonce

--- src/siwe_controller.go
+++ src/siwe_controller.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/lithammer/shortuuid/v4"
-	"github.com/rs/zerolog/log"
 	"github.com/spruceid/siwe-go"
 	"net/http"
 )
@@ -15,6 +13,12 @@ type Nonce struct {
 	gorm.Model
 	Nonce   string `gorm:"uniqueIndex"`
 	Address string
+}
+
+// VerifyNonceRequest json
+type VerifyNonceRequest struct {
+	Message   string `json:"message"`
+	Signature string `json:"signature"`
 }
 
 func getNonce(db *gorm.DB) func(echo.Context) error {
@@ -32,13 +36,35 @@ func getNonce(db *gorm.DB) func(echo.Context) error {
 func verifyNonce(db *gorm.DB) func(echo.Context) error {
 	return func(c echo.Context) error {
 
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(c.Request().Body)
-		raw := buf.String()
-		log.Printf("got: %v", raw)
+		var verifyNonceRequest VerifyNonceRequest
+		err := c.Bind(&verifyNonceRequest)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
 
-		message, err := siwe.ParseMessage(raw)
-		log.Printf("parsed: %v %v", message, err)
-		return c.String(http.StatusBadRequest, "")
+		message, err := siwe.ParseMessage(verifyNonceRequest.Message)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		var nonce Nonce
+		db.Where("nonce=?", message.GetNonce()).First(&nonce)
+		if nonce.Nonce != message.GetNonce() {
+			return c.String(http.StatusNotFound, "")
+		}
+
+		if nonce.Address != "" {
+			return c.String(http.StatusBadRequest, "used nonce")
+		}
+
+		_, err = message.VerifyEIP191(verifyNonceRequest.Signature)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		nonce.Address = message.GetAddress().String()
+		db.Save(&nonce)
+
+		return c.String(http.StatusOK, "")
 	}
 }
